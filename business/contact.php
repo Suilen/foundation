6aab682f0c8c41cbcf8bafb72b641ba831bf3d19
suilen <zeldazheng@gmail.com>
2014-10-15 10:39:46 -0400

    set up

---
+++ business/contact.php
@@ -0,0 +1,15 @@
+<!-- INCLUDE HEAD -->
+<?php include("includes/Head.html"); ?>
+<!-- INCLUDE primary nav -->
+<?php include("includes/MainNav.html"); ?>
+
+<!-- VIEW START HERE -->
+
+<?php include("views/v-contact.html"); ?>
+
+<!-- VIEW END HERE -->
+
+<!-- INCLUDE footer -->
+<?php include("includes/Footer.html"); ?>
+<!-- INCLUDE FOOT -->	
+<?php include("includes/Foot.html"); ?>
